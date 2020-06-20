6251c70327d1da1fbae8e6189c415928b6eb1aba
Benny Pollak <yo372002@yahoo.com>
2020-06-20 13:49:49 -0400

    Initial Commit

---
+++ dcw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dcw1
+//
+//  Created by Benny Pollak on 6/20/20.
+//  Copyright © 2020 Alben Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dcw1Tests/dcw1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  dcw1Tests.swift
+//  dcw1Tests
+//
+//  Created by Benny Pollak on 6/20/20.
+//  Copyright © 2020 Alben Software. All rights reserved.
+//
+
+import XCTest
+@testable import dcw1
+
+class dcw1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
