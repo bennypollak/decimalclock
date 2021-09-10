e3d6ece1a0b8c287b4adf83d0193473df6a45e08
Benny Pollak <yo372002@yahoo.com>
2021-09-10 19:12:45 -0400

    Add gestures to watch app

--- ww Extension/ContentView.swift
+++ ww Extension/ContentView.swift
@@ -11,8 +11,8 @@ struct AWClocks : View {
     var timeParts: TimeParts
     var body: some View {
         VStack(spacing:0) {
-            Text(Date.timeParts(decimal: timeParts.decimal, hex: timeParts.hex).string).font(Font.body.monospacedDigit())
-            BinaryClock(timeParts: Date.timeParts(decimal: timeParts.decimal, hex: timeParts.hex))
+            Text(Date.timeParts(decimal: timeParts.decimal, hex: timeParts.hex, reverse: timeParts.reverse).string).font(Font.body.monospacedDigit())
+            BinaryClock(timeParts: Date.timeParts(decimal: timeParts.decimal, hex: timeParts.hex, reverse: timeParts.reverse))
         }
     }
 }
@@ -20,14 +20,32 @@ struct AWClocks : View {
 struct ContentView: View {
     let timer = Timer.publish(every: 0.007, on: .main, in: .common).autoconnect()
     @State var time = "time"
+    @State private var hex = true
+    @State private var reverse = false
+    var gesture: some Gesture {
+        DragGesture()
+            .onChanged { drag in
+        }
+        .onEnded { drag in
+            let w = drag.translation.width
+            if w > 50 {
+                self.reverse = false
+            } else if w < -50 {
+                self.reverse = true
+            }
+        }
+    }
     var body: some View {
         VStack(spacing: 1) {
             Text("\(time)").font(Font.body.monospacedDigit()).frame(width: 0, height: 0, alignment: .top)
-            AWClocks(timeParts: Date.timeParts(decimal: true, hex: true))
-            AWClocks(timeParts: Date.timeParts(decimal: false))
+            AWClocks(timeParts: Date.timeParts(decimal: true, hex: self.hex, reverse: self.reverse))
+            AWClocks(timeParts: Date.timeParts(decimal: false, reverse: self.reverse))
         }.onReceive(timer) { input in
-            self.time = Date.timeParts(decimal: true, hex: true).string
-        }
+            self.time = Date.timeParts(decimal: true, hex: true, reverse: self.reverse).string
+        }.onTapGesture(count: 2) {
+            self.hex = !self.hex
+        }.gesture(self.gesture)
+        
     }
 }
 
